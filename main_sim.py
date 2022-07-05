0f354da6462fe63196fda924664798a9bf2e897b
Prakyath Kantharaju <prakyathk33@gmail.com>
2022-07-05 13:59:49 -0500

    gettitng global range and position

--- main_sim.py
+++ main_sim.py
@@ -41,11 +41,11 @@ def main():
 
     # starting off with the trot gait.
     sim_state = pupper.sim.get_state()
-    
+
     ids = [pupper.sim.model.get_joint_qpos_addr(c) for c in pupper.sim.model.actuator_names]
     initial_start_positions = state.joint_angles.flatten('F')
     pupper.data.ctrl[:] = initial_start_positions
-    
+
 
     # setting the angle in the state
     for i in range(len(ids)):
@@ -60,26 +60,27 @@ def main():
         if any(previous_command != state.joint_angles.flatten('F')) and i > 20:
             # print(act_time - pupper.time)
             act_time = pupper.time
-            
+
             pupper.data.ctrl[:] = state.joint_angles.flatten('F')
             # print(state.joint_angles.flatten('F'))
             previous_command = state.joint_angles.flatten('F')
             if i > 100:
-                command.horizontal_velocity = np.array([0.1, 0])
+                command.horizontal_velocity = np.array([0.2, 0])
                 error = 0 - pupper.sim.data.body_xpos[1][1]
-                print(pupper.sim.data.get_mocap_pos('torso'))
+                print('velcoity', pupper.sim.data.body_xvelp[2, :])
+                print('position', pupper.sim.data.get_body_xpos("torso"))
                 # print(pupper.sim.data.body_xpos[1])
                 # print("error:", error)
-                command.yaw_rate = np.clip(error, a_min=-0.3, a_max=0.3)
+                #command.yaw_rate = np.clip(error, a_min=-0.3, a_max=0.3)
                 # command.yaw_rate = 0.1
-                command.height = -0.1
+                command.height = -0.15
                 # command.height = -0.2
-            
-        controller.run(state, command)
-        
-    
 
-    
+        controller.run(state, command)
+
+
+
+
 
     start_time = pupper.time
     start = True
